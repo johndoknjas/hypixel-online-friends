afed07e9a318f7ff0e199cf00ccb38f363afcc8b
johndoknjas <jdoknjas@sfu.ca>
2023-01-19 17:51:33 -0800

    Move up if statement earlier in the control flow.

--- main.py
+++ main.py
@@ -85,6 +85,8 @@ def main():
     hypixel.set_api_keys()
 
     args = Args(sys.argv)
+    if args.find_matching_igns_or_uuids_in_results():
+        ProcessingResults.print_all_matching_uuids_or_igns(args.get_args(True, True)[0])
     players_from_args = get_players_from_args(args)
     diff_f_lists(players_from_args, args)
     player = combine_players(players_from_args)
@@ -97,9 +99,6 @@ def main():
 
     if args.update_uuids():
         Files.update_uuids_file(ProcessingResults.ign_uuid_pairs_in_results())
-    
-    if args.find_matching_igns_or_uuids_in_results():
-        ProcessingResults.print_all_matching_uuids_or_igns(args.get_args(True, True)[0])
 
     report = player.create_dictionary_report(sort_final_result_by_fkdr = not args.sort_by_star())
     if args.do_file_output():
