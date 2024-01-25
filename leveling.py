94f6b6f8de858646eb0ba1886bd5e6303e7fafcb
johndoknjas <jdoknjas@sfu.ca>
2024-01-24 22:36:29 -0800

    Add some tests for leveling.py. Also in getTotalExpToLevelFloor, assert that the float is equal to its int cast, and then return said cast.

--- leveling.py
+++ leveling.py
@@ -13,19 +13,21 @@ _QUADRATIC_FUNC = QuadraticFunc(_GROWTH/2, _BASE - 1.5*_GROWTH, _GROWTH - _BASE,
    object should only be used to find the floor. Conversely for finding xp, only int values
    for level should be supplied."""
 
-def getExactLevel(exp: float) -> float:
-    return getLevelFloor(exp) + getPercentageToNextLevel(exp)
+def getTotalExpToLevelFloor(level: int) -> int:
+    xp = _QUADRATIC_FUNC.y_val(level)
+    assert int(xp) == xp
+    return int(xp)
+
+def getExpFromLevelToNext(level: int) -> int:
+    return getTotalExpToLevelFloor(level+1) - getTotalExpToLevelFloor(level)
 
 def getLevelFloor(exp: float) -> int:
     return floor(next(l for l in _QUADRATIC_FUNC.x_vals(exp) if l >= 1))
 
-def getPercentageToNextLevel(exp: float):
-    lv = getLevelFloor(exp)
-    xp_earned_during_current_level = exp - getTotalExpToLevelFloor(lv)
-    return xp_earned_during_current_level / getExpFromLevelToNext(lv)
+def getExactLevel(exp: float) -> float:
+    return getLevelFloor(exp) + getPercentageToNextLevel(exp)
 
-def getTotalExpToLevelFloor(level: int) -> float:
-    return _QUADRATIC_FUNC.y_val(level)
-
-def getExpFromLevelToNext(level: int):
-    return getTotalExpToLevelFloor(level+1) - getTotalExpToLevelFloor(level)
+def getPercentageToNextLevel(exp: float) -> float:
+    lvl = getLevelFloor(exp)
+    xp_earned_during_current_level = exp - getTotalExpToLevelFloor(lvl)
+    return xp_earned_during_current_level / getExpFromLevelToNext(lvl)
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pytest
 
 from MyClasses import Specs
+import leveling
 
 Specs.set_common_specs(False, False)
 
@@ -14,4 +15,29 @@ class Tests:
         specs = Specs(False, False, friends_specs, 0)
         specs._friends_specs._just_uuids = True
         assert not friends_specs._just_uuids
-        # specs.print_fields()
+    
+    def test_network_levels(self):
+        level_range = range(1, 10000)
+        xps = [leveling.getTotalExpToLevelFloor(l) for l in level_range]
+        assert xps[0] == 0 and xps[1] == 10000 and xps[2] == 22500 and xps[-1] == 125037487500
+        assert all(isinstance(xp, int) for xp in xps)
+        for i, xp in enumerate(xps):
+            lvl = level_range[i]
+            assert leveling.getLevelFloor(xp) == lvl
+            if xp > 0:
+                assert leveling.getLevelFloor(xp-1) == lvl-1
+            assert leveling.getLevelFloor(xp+1) == lvl
+
+    def test_leveling_errors(self):
+        with pytest.raises(AssertionError):
+            leveling.getLevelFloor(0.0001)
+        with pytest.raises(AssertionError):
+            leveling.getLevelFloor(10000-0.0001)
+        with pytest.raises(AssertionError):
+            leveling.getTotalExpToLevelFloor(0)
+        with pytest.raises(AssertionError):
+            leveling.getTotalExpToLevelFloor(10000)
+        with pytest.raises(AssertionError):
+            leveling.getTotalExpToLevelFloor(1.00001)
+        with pytest.raises(AssertionError):
+            leveling.getTotalExpToLevelFloor(1.9999)
